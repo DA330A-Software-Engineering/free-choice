389cb3d56efd0be7a68c114ff44b632e18550376
oscar karlsson <oscar.karl00@hotmail.com>
2023-03-06 12:25:59 +0100

    Login / Signup View

---
+++ src/views/Login.view.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+/** Login View */
+const LoginView: FC = () => {
+    return (
+        <>
+          <p>Login View</p>
+        </>
+    )
+}
+
+// Export the component
+export default LoginView
