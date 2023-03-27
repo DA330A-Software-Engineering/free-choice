302af14916abf2f76f9f80887f7aa775a02d364f
Hampus Andersson <haampusandersson@live.se>
2023-03-27 20:43:13 +0200

    unused code

--- src/components/devices/ToggleDevice.style.ts
+++ src/components/devices/ToggleDevice.style.ts
@@ -7,19 +7,3 @@ export const toggleDeviceStyle = css`
   align-items: center;
   margin: 0.5rem;
 `;
-
-export const toggleButtonStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const toggleIconStyle = css`
-  font-size: 3rem;
-`;
-
-export const toggleStatusStyle = css`
-  font-size: 1.5rem;
-  margin-top: 0.5rem;
-  text-align: center;
-`;
