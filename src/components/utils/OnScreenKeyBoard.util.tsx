1267ef8e2542bf0f133045182ed97a231b4c33df
Hampus Andersson <haampusandersson@live.se>
2023-05-24 21:14:10 +0200

    added onscreenkeyboard to login

--- src/components/utils/OnScreenKeyBoard.util.tsx
+++ src/components/utils/OnScreenKeyBoard.util.tsx
@@ -1,6 +1,6 @@
 // OnScreenKeyboard.util.tsx
 
-import React, { FC, useState, useCallback } from "react";
+import React, { FC, useState, useCallback, useEffect } from "react";
 import SimpleKeyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
@@ -20,26 +20,28 @@ const OnScreenKeyboard: FC<OnScreenKeyboardProps> = ({
   const [layoutName, setLayoutName] = useState("default");
   const [currentInput, setCurrentInput] = useState(inputValue);
 
+  useEffect(() => {
+    setCurrentInput(inputValue);
+  }, [inputValue]);
+
   const handleShift = () => {
     setLayoutName(layoutName === "default" ? "shift" : "default");
   };
 
   const handleKeyPress = useCallback(
     (button: string) => {
+      let newValue = currentInput; // new line
       if (button === "{shift}" || button === "{lock}") {
         handleShift();
       } else if (button === "{bksp}") {
-        setCurrentInput(currentInput.slice(0, -1));
-        onInput(currentInput.slice(0, -1));
+        newValue = currentInput.slice(0, -1);
       } else if (button === "{space}") {
-        setCurrentInput(currentInput + " ");
-        onInput(currentInput + " ");
-      } else if (button === "{enter}") {
-        // handle enter key press if needed
+        newValue = currentInput + " ";
       } else if (button.length === 1) {
-        setCurrentInput(currentInput + button);
-        onInput(currentInput + button);
+        newValue = currentInput + button;
       }
+      setCurrentInput(newValue);
+      onInput(newValue);
     },
     [layoutName, currentInput, onInput]
   );
--- src/components/views/Login.view.tsx
+++ src/components/views/Login.view.tsx
@@ -1,14 +1,38 @@
-import { FC, useState } from 'react';
-import { IAPIResponse, useAuth } from '../../contexts/AuthContext';
-import Input from '../interactable/Input.cmpt';
+import { FC, useState } from "react";
+import { IAPIResponse, useAuth } from "../../contexts/AuthContext";
+import Input from "../interactable/Input.cmpt";
+import OnScreenKeyboard from "../utils/OnScreenKeyBoard.util";
 
 /** Login View */
 const LoginView: FC = () => {
-
   const auth = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
 
-  const [email, SetEmail] = useState("");
-  const [password, SetPassword] = useState("");
+  const handleEmailInputChange = (input: string) => {
+    setEmail(input);
+  };
+
+  const handlePasswordInputChange = (input: string) => {
+    setPassword(input);
+  };
+
+  const handleEmailInputFocus = () => {
+    setEmailFocused(true);
+    setPasswordFocused(false);
+  };
+
+  const handlePasswordInputFocus = () => {
+    setEmailFocused(false);
+    setPasswordFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setEmailFocused(false);
+    setPasswordFocused(false);
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,18 +44,41 @@ const LoginView: FC = () => {
   };
 
   return (
-      <>
-        <div className='authWrapper'>
-          <h1>Login View</h1>
-          <form onSubmit={handleSubmit}>
-            <Input inputStyle='authInput' placeholder='email..' onChange={(email: string) => SetEmail(email)}  />
-            <Input type='password' inputStyle='authInput' placeholder='password..' onChange={(password: string) => SetPassword(password)}  />
-            <button>Login</button>
-          </form>
+    <>
+      <div className="authWrapper">
+        <h1>Login View</h1>
+        <form onSubmit={handleSubmit}>
+          <Input
+            inputStyle="authInput"
+            placeholder="email.."
+            value={email}
+            onFocus={handleEmailInputFocus}
+            onChange={handleEmailInputChange}
+          />
+          <Input
+            type="password"
+            inputStyle="authInput"
+            placeholder="password.."
+            value={password}
+            onFocus={handlePasswordInputFocus}
+            onChange={handlePasswordInputChange}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+      {(emailFocused || passwordFocused) && (
+        <div className="keyboardContainer">
+          <OnScreenKeyboard
+            onInput={
+              emailFocused ? handleEmailInputChange : handlePasswordInputChange
+            }
+            onBlur={handleInputBlur}
+            inputValue={emailFocused ? email : password}
+          />
         </div>
-      </>
-  )
-}
+      )}
+    </>
+  );
+};
 
-// Export the component
-export default LoginView
+export default LoginView;
